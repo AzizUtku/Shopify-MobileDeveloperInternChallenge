0ee6acc78f1a2764f873a6322575d87fdc556c15
azizutku <azizutku@gmail.com>
2019-01-20 02:15:25 +0300

    Checks internet connection.

--- app/src/main/java/com/azutka/shopify_challenge/StartActivity.java
+++ app/src/main/java/com/azutka/shopify_challenge/StartActivity.java
@@ -1,12 +1,19 @@
 package com.azutka.shopify_challenge;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import com.azutka.shopify_challenge.interfaces.OnTaskCompletedListener;
 import com.azutka.shopify_challenge.models.Collection;
 import com.azutka.shopify_challenge.requests.GeneralRequest;
+import com.azutka.shopify_challenge.utils.Constant;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +25,24 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        getCollections();
+        ProgressBar pb = findViewById(R.id.start_pb);
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if(networkInfo != null && networkInfo.isConnectedOrConnecting()){
+            getCollections();
+        } else {
+            AlertDialog.Builder dialogAbout = new AlertDialog.Builder(this);
+            dialogAbout.setCancelable(false);
+            dialogAbout.setTitle("No connection");
+            dialogAbout.setMessage("Internet connection is needed!");
+            dialogAbout.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            dialogAbout.show();
+        }
     }
 
     public void getCollections() {
